9c64f3034198834c18b7bfce1bb0b58bfe67b4e7
SilencedFrost <thnrgbefv0987@gmail.com>
2026-02-10 10:37:58 +0700

    feature/ added browser parameter support to BaseTest

--- Selenium/TestCases/Railway/BaseTest.java
+++ Selenium/TestCases/Railway/BaseTest.java
@@ -2,20 +2,28 @@ package TestCases.Railway;
 
 import Common.Constant.Constant;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+    @Parameters("browser")
     @BeforeMethod
-    public void beforeMethod() {
+    public void beforeMethod(@Optional("chrome") String browser) {
         log.info("Pre-condition");
 
-        Constant.WEBDRIVER = new ChromeDriver();
+        Constant.WEBDRIVER = switch(browser) {
+            case "chrome" -> new ChromeDriver();
+            case "firefox" -> new FirefoxDriver();
+            default -> throw new RuntimeException("Unsupported browser: " + browser);
+        };
         Constant.WEBDRIVER.manage().window().maximize();
     }
 
